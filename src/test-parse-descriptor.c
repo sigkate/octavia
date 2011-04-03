0e93b532cb2bb6a490a5bc6c357f65edc59d7ae7
Chris Palmer <chris@noncombatant.org>
2011-04-03 15:35:01 -0500

    Toy to exercise parse_descriptor.

---
+++ src/test-parse-descriptor.c
@@ -0,0 +1,36 @@
+#include <sys/stat.h>
+
+#include <err.h>
+#include <stdio.h>
+
+#include "descriptor.h"
+#include "util.h"
+
+
+dynamic char * get_data(const char * pathname) {
+      struct stat stt;
+
+      if (stat(pathname, &stt))
+            err(1, "%s", pathname);
+
+      char * data = allocate(stt.st_size, 1);
+
+      FILE * fl = fopen(pathname, "r");
+      (void) fread(data, 1, stt.st_size, fl);
+      fclose(fl);
+      return data;
+}
+
+
+int main(int count, char * arguments []) {
+      for (int i = 1; i < count; i++) {
+            char * data = get_data(arguments[i]);
+            Descriptor * d = parse_descriptor(data);
+            printf("%p\n", d);
+            free(data);
+            free(d);
+      }
+
+      return 0;
+}
+
